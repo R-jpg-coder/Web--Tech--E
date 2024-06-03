8c05d9331280995de1e47bd5ba35bab0dfe9d65b
R-jpg-coder <72785465+R-jpg-coder@users.noreply.github.com>
2024-06-03 16:01:34 +0600

    Add files via upload
    It is movie type java code

---
+++ Constant.java
@@ -0,0 +1,7 @@
+package com.user.Constant;
+
+public class Constant {
+  public static final String FORGET_PASSWORD_TEMPLATE = "forget-password.html";
+  public static final String NEW_ACCOUNT_TEMPLATE = "new-account-reset-password.html";
+
+}
